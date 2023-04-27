82fbb23aa7a3d8eacfe05536f53793b407c05664
mrbll48 <william.marks87@gmail.com>
2023-04-26 20:32:19 -0700

    removed old CSS that was commented out

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
 var workoutAPIKey = '4Z7299Xd9HEZMOuF2j15sg==HS0gwsLVKjmqzWlK'
 var youtubeAPIKey = 'AIzaSyC7M2WXOg4tv1C2qqm52Qn_ePVce1tHqDA'
 
 function getWorkout(event) {
     event.preventDefault();
-    var exerciseRequestUrl = `https://api.api-ninjas.com/v1/exercises?type=${type}&muscle=${muscle}&difficulty=${difficulty}&X-Api-Key=$${workoutAPIKey}`
+    var exerciseRequestUrl = `https://api.api-ninjas.com/v1/exercises?type=${type}&muscle=${muscle}&difficulty=${difficulty}&X-Api-Key=${workoutAPIKey}`
     fetch(exerciseRequestUrl)
         .then(function (response) {
             return response.json();
@@ -24,3 +29,4 @@ function getVideo() {
                 console.log(data)
             })
 }
+
