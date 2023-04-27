7f2bc453844d97cbfe3eb974a261139dc19c843d
mrbll48 <william.marks87@gmail.com>
2023-04-26 20:42:39 -0700

    created variables for dropdown inputs, created 2 more dropdowns in HTML

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems, options);
   });
-
+// API keys for fetch requests
 var workoutAPIKey = '4Z7299Xd9HEZMOuF2j15sg==HS0gwsLVKjmqzWlK'
 var youtubeAPIKey = 'AIzaSyC7M2WXOg4tv1C2qqm52Qn_ePVce1tHqDA'
+// tag generate workout button
+var generateBtn = document.getElementById('generate-button')
+// tag the dropdown menus for each option
+var workoutType = document.getElementById('workout-type')
+var workoutMuscle = document.getElementById('workout-muscle')
+var workoutDifficulty = document.getElementById('workout-difficulty')
 
 function getWorkout(event) {
     event.preventDefault();
-    var exerciseRequestUrl = `https://api.api-ninjas.com/v1/exercises?type=${type}&muscle=${muscle}&difficulty=${difficulty}&X-Api-Key=${workoutAPIKey}`
+    var exerciseRequestUrl = `https://api.api-ninjas.com/v1/exercises?type=${workoutType}&muscle=${workoutMuscle}&difficulty=${workoutDifficulty}&X-Api-Key=${workoutAPIKey}`
     fetch(exerciseRequestUrl)
         .then(function (response) {
             return response.json();
@@ -16,6 +22,7 @@ function getWorkout(event) {
             .then(function (data) {
                 console.log(data);
             })
+            getVideo()
 }
 
 function getVideo() {
@@ -30,3 +37,4 @@ function getVideo() {
             })
 }
 
+generateBtn.addEventListener('click', getWorkout)
