32659a4e4a0fca9e573e5ea9217fb7142804096e
Chris Vithaya <parker@Chriss-MacBook-Air.local>
2023-05-01 19:13:45 -0700

    adding code for youtube API

--- assets/script.js
+++ assets/script.js
@@ -37,3 +37,36 @@ document.addEventListener('DOMContentLoaded', function() {
 
 generateBtn.addEventListener('click', getWorkout)
 
+// Section for youtube API
+
+function retrieveMyUploads() {
+    var results = YouTube.Channels.list('contentDetails', {mine: true});
+    for(var i in results.items) {
+      var item = results.items[i];
+      // Get the playlist ID, which is nested in contentDetails, as described in the
+      // Channel resource: https://developers.google.com/youtube/v3/docs/channels
+      var playlistId = item.contentDetails.relatedPlaylists.uploads;
+  
+      var nextPageToken = '';
+  
+      // This loop retrieves a set of playlist items and checks the nextPageToken in the
+      // response to determine whether the list contains additional items. It repeats that process
+      // until it has retrieved all of the items in the list.
+      while (nextPageToken != null) {
+        var playlistResponse = YouTube.PlaylistItems.list('snippet', {
+          playlistId: playlistId,
+          maxResults: 25,
+          pageToken: nextPageToken
+        });
+  
+        for (var j = 0; j < playlistResponse.items.length; j++) {
+          var playlistItem = playlistResponse.items[j];
+          Logger.log('[%s] Title: %s',
+                     playlistItem.snippet.resourceId.videoId,
+                     playlistItem.snippet.title);
+  
+        }
+        nextPageToken = playlistResponse.nextPageToken;
+      }
+    }
+  }
