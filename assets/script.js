d0e473699bea6bd555de9c62c7d0c7caa7197ef4
Chris Vithaya <parker@Chriss-MacBook-Air.local>
2023-05-01 20:36:22 -0700

    altered code

--- assets/script.js
+++ assets/script.js
@@ -81,3 +81,41 @@ function searchByTopic() {
       Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
     }
   }
+
+//  Attempt to search for youtube channel
+
+<script src="https://apis.google.com/js/api.js"></script>
+
+function authenticate(){
+    return Gapi.auth2.getAuthInstance()
+    .signIn({scope: "https://www.googleapis.com/auth/youtube.readonly"})
+    .then(function() { console.log("Sign-in successful"); },
+        function(err) { console.error("Error signing in", err); });
+}
+
+function loadClient() {
+    gapi.client.setApiKey("AIzaSyCuRgrGBysEVLr-VF8kdTB1fvWRPRgumhI");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+
+// Make sure the client is loaded and sign-in is complete before calling this method.
+function execute() {
+    return gapi.client.youtube.channels.list({
+      "part": [
+        "snippet,contentDetails,statistics"
+      ],
+      "forUsername": "GoogleDevelopers"
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+
+  gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
+  });
+
